600c517754c6c0c3eb3bcc4475c9eb8e1d0c9a2f
Robert-C-Warren <robertcwarren1@gmail.com>
2025-01-16 10:14:23 -0500

    api.js change

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://127.0.0.1:5000/'});
+const API = axios.create({ baseURL: 'http://35.222.106.130:5000/'});
 
 export default API;
