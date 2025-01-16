2822273aa5fe8d6f4a0b40f2341edbe5caf213de
Robert-C-Warren <robertcwarren1@gmail.com>
2025-01-16 09:01:51 -0500

    config

--- backend/config.py
+++ backend/config.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 import os
 
 def get_database():
-    mongo_uri = r"mongodb+srv://robertcwarren1:Sn3akyHunt//&r1995@eco-commerce.ktcjv.mongodb.net/eco-commerce"
+    mongo_uri = "mongodb+srv://robertcwarren1:Sn3akyHunt%26r1995@eco-commerce.ktcjv.mongodb.net/eco-commerce"
     client = MongoClient(mongo_uri)
     db = client["eco-commerce"]
     return db
