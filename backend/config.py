7a7500adaf89dd6e7bf4857bdcde308045e23d78
Robert-C-Warren <robertcwarren1@gmail.com>
2025-01-17 09:33:07 -0500

    git ignore

--- backend/config.py
+++ backend/config.py
@@ -1,8 +1,14 @@
 from pymongo import MongoClient
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+port = os.getenv("PORT", 5000)
+mongo_uri = os.getenv("MONGO_URI")
+allowed_origins = os.getenv("ALLOWED_ORIGINS").split(",")
 
 def get_database():
-    mongo_uri = "mongodb+srv://robertcwarren1:Sn3akyHunt%26r1995@eco-commerce.ktcjv.mongodb.net/eco-commerce"
     client = MongoClient(mongo_uri)
     db = client["eco_commerce"]
     return db
