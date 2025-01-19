e5209b7e8a4b77c75f0050017fc21d317c14458d
Robert-C-Warren <robertcwarren1@gmail.com>
2025-01-18 21:00:09 -0500

    Image UPdate

--- frontend/src/pages/Navbar.js
+++ frontend/src/pages/Navbar.js
@@ -1,7 +1,7 @@
 import { React } from "react";
 import "./ProductList.css"
 import { Link, useNavigate } from "react-router-dom"
-import smallLogo from "../resources/ECLogo.svg";
+import smallLogo from "../resources/eclogov2.svg";
 import DarkModeToggle from "./DarkModeToggle";
 import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
